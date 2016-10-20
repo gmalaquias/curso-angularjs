589ef329562e05ef7ee434018fcf6fe32e86c0e3
Gabriel Malaquias <gemalaquias@gmail.com>
2016-10-20 16:32:12 -0200

    Aulas 20 e 21

--- assets/js/aula20.controller.js
+++ assets/js/aula20.controller.js
@@ -1,5 +1,29 @@
-app.controller('aula20controller', ['$scope', function($scope){
+var app = angular.module('app', ['pascalprecht.translate']);
 
+app.config(['$translateProvider',function($translateProvider) {
 	
+	$translateProvider.translations('pt-br',{
+		TITULO : "Seja bem vindo",
+		PARAGRAFO1 : "Essa é uma página de inicio",
+		PTBR_BTN : "PT BR",
+		EN_BTN : "English"
+	});
 
+	$translateProvider.translations('en',{
+		TITULO : "Welcome",
+		PARAGRAFO1 : "This is a hello pagina",
+		PTBR_BTN : "Portugues",
+		EN_BTN : "Inglês"
+	});
+
+	$translateProvider.preferredLanguage('pt-br');
+
+}])
+
+app.controller('aula20controller', ['$scope','$translate', function($scope, $translate){
+
+	$scope.setaIdioma = function(sigla){
+		$translate.use(sigla);
+	}
+	
 }]);
---
+++ assets/js/aula22.controller.js
@@ -0,0 +1,7 @@
+var app = angular.module('app', []);
+
+app.controller('aula22controller', ['$scope', function($scope){
+
+	$scope.mensagem = "asdasd";
+	
+}]);
